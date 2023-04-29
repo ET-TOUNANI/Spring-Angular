398321c85c97b6638443c52c2153609f30a61973
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2023-04-29 10:13:23 +0100

    fit : add products component

--- FirstTry/src/app/app.module.ts
+++ FirstTry/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
