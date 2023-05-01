6b60346148f942e460cf9b24db345d3ffbe33aa5
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2023-05-01 16:07:52 +0100

    Digital Banking add back end initialisation

---
+++ DigitalBanking/backEnd_spring/src/test/java/com/ettounani/backend_digitalbanking/BackEndDigitalBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ettounani.backend_digitalbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndDigitalBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
