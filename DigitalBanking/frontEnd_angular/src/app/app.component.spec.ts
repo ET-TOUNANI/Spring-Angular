8a6697a4e65a5774914be887bc46662f1660b468
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2023-05-14 01:45:49 +0100

    add ui

--- DigitalBanking/frontEnd_angular/src/app/app.component.spec.ts
+++ DigitalBanking/frontEnd_angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DigitalBanking'`, () => {
+  it(`should have as title 'digital-banking-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DigitalBanking');
+    expect(app.title).toEqual('digital-banking-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('DigitalBanking app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('digital-banking-web app is running!');
   });
 });
